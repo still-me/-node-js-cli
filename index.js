f1047711ed6fc8ecc0ee899d4cea4b5134426c6e
Fomenko Vladyslav <fomenkovlad93@gmail.com>
2021-08-13 17:39:55 +0300

    add comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-disable space-before-function-paren */
 const { Command } = require('commander');
 const program = new Command();
+//* or const {program} = require('commander');
 
 const {
   listContacts,
@@ -9,6 +10,7 @@ const {
   addContact,
 } = require('./contacts.js');
 
+//* options its commands ('short, full <type> if not boolean', command name)
 program
   .option('-a, --action <type>', 'choose action')
   .option('-i, --id <type>', 'user id')
@@ -16,9 +18,12 @@ program
   .option('-e, --email <type>', 'user email')
   .option('-p, --phone <type>', 'user phone');
 
+//* add array of commands from command line
 program.parse(process.argv);
 
+//* create obj with keys for example --action get --id 5 = {action: 'get', id: '5'}
 const argv = program.opts();
+console.log(argv);
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
